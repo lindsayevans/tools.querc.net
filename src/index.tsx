18329448782c7ae52698d7748e519d6e6b3a239a
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-05-07 10:09:21 +1000

    feat: React app setup

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const $app = document.getElementById('app');
+
+if ($app) {
+  const root = createRoot($app);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
