352e2e62567ed50d29850a57138c90327ed08bc1
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-05-09 09:11:58 +1000

    feat: View Source

---
+++ src/ProgramFiles/ViewSource/ViewSource.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Application } from '../../windows/system32';
+
+import './ViewSource.scss';
+
+export const ViewSource = () => {
+  const [url, setUrl] = useState<string>('https://boomworks.com.au/');
+
+  const viewSource = () => {
+    window.open(`view-source:${url}`, '_blank');
+  };
+
+  return (
+    <>
+      <Application
+        executableName="ViewSource.exe"
+        iconSrc="/icons/ViewSource.ico"
+        title="View Source"
+        width={600}
+        height={110}
+      >
+        <div className="form">
+          <div className="field-row-stacked">
+            <label htmlFor="url">URL</label>
+            <input
+              type="url"
+              id="url"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              onFocus={(e) => e.target.select()}
+            />
+          </div>
+          <button type="button" onClick={() => viewSource()}>
+            View source
+          </button>
+        </div>
+        <p>
+          <a href={`view-source:${url}`} target="_blank">
+            View source
+          </a>
+        </p>
+      </Application>
+    </>
+  );
+};
