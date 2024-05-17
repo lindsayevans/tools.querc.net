d99281d88a018852f73bd9e298e4216ebaf4d006
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-05-17 15:17:47 +1000

    feat: hamburger menu

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 
 import './Layout.scss';
+import { classList } from '../components/classList';
 
 export const Layout = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header>
         <h1>
           <Link to="/">tools.querc.net</Link>
         </h1>
-        <nav>
+        <nav className={classList([menuOpen ? 'open' : undefined])}>
+          <button
+            type="button"
+            className="hamburger"
+            onClick={() => setMenuOpen(!menuOpen)}
+            title={menuOpen ? 'Hide menu' : 'Show menu'}
+          >
+            🍔
+          </button>
           <ul>
             <li>
               <NavLink
@@ -18,6 +29,7 @@ export const Layout = () => {
                 className={({ isActive, isPending }) =>
                   isActive ? 'active' : isPending ? 'pending' : ''
                 }
+                onClick={() => setMenuOpen(false)}
               >
                 Base64 converter
               </NavLink>
@@ -28,6 +40,7 @@ export const Layout = () => {
                 className={({ isActive, isPending }) =>
                   isActive ? 'active' : isPending ? 'pending' : ''
                 }
+                onClick={() => setMenuOpen(false)}
               >
                 DateTimeFormat playground
               </NavLink>
